dcce00d0d45ea41a743214894b7b760d6d5f478e
RGbrunofvm <programovil2022@gmail.com>
2024-04-17 19:37:23 -0400

    Examples 9-12

---
+++ Git-UnitTestTdd/src/test/java/sessions/brunoVasquez/katas/example10/Kata.java
@@ -0,0 +1,15 @@
+package sessions.brunoVasquez.katas.example10;
+
+import java.util.Arrays;
+
+public class Kata {
+
+    public static int findShort(String s) {
+        return 0;
+    }
+
+    public static int findShortUsingStream(String s) {
+        // Split the string into an array of words, then find the minimum length using streams
+        return 0;
+    }
+}
---
+++ Git-UnitTestTdd/src/test/java/sessions/brunoVasquez/katas/example11/Kata.java
@@ -0,0 +1,16 @@
+package sessions.brunoVasquez.katas.example11;
+
+import java.util.Arrays;
+
+public class Kata {
+
+    public static int findSmallestInt(int[] array) {
+       return 0;
+    }
+
+    public static int findSmallestIntUsingStream(int[] array) {
+        // Find the smallest integer using streams
+        return 0;
+    }
+
+}
