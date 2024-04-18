94f00000770fe70ac29fed125d540344aa5517ee
RGbrunofvm <programovil2022@gmail.com>
2024-04-17 21:56:33 -0400

    Adding Methods

--- Git-UnitTestTdd/src/test/java/sessions/brunoVasquez/katas/example10/Kata.java
+++ Git-UnitTestTdd/src/test/java/sessions/brunoVasquez/katas/example10/Kata.java
@@ -4,13 +4,23 @@ import java.util.Arrays;
 
 public class Kata {
 
-    public static int findShort(String s) {
+    public static int findShort(String stringWords) {
         // Apply Split to string
-        return 0;
+        String[] words = stringWords.split(" ");
+        int shortest = Integer.MAX_VALUE;
+        for (String word : words) {
+            if (word.length() < shortest) {
+                shortest = word.length();
+            }
+        }
+        return shortest;
     }
 
-    public static int findShortUsingStream(String s) {
+    public static int findShortUsingStream(String stringWords) {
         // Split the string into an array of words, then find the minimum length using streams
-        return 0;
+        return Arrays.stream(stringWords.split(" "))
+                .mapToInt(String::length)
+                .min()
+                .getAsInt();
     }
 }
--- Git-UnitTestTdd/src/test/java/sessions/brunoVasquez/katas/example11/Kata.java
+++ Git-UnitTestTdd/src/test/java/sessions/brunoVasquez/katas/example11/Kata.java
@@ -1,16 +1,30 @@
 package sessions.brunoVasquez.katas.example11;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Kata {
 
+    /**
+     *
+     * @param array
+     * @return
+     */
     public static int findSmallestInt(int[] array) {
-       return 0;
+        int smallest = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        return smallest;
     }
 
     public static int findSmallestIntUsingStream(int[] array) {
         // Find the smallest integer using streams
-        return 0;
+        return Arrays.stream(array)
+                .min()
+                .getAsInt();
     }
 
 }
