8ad79ad83bf0777ead35a2394f7f987be31e077a
victorcarrasco <vhcarrascof@gmail.com>
2024-04-29 11:36:23 -0400

    started trello automation

---
+++ Git-UnitTestTdd/src/test/java/com/trello/boards.java
@@ -0,0 +1,12 @@
+package com.trello;
+
+import org.testng.annotations.Test;
+
+public class boards {
+
+    @Test
+
+    public void test_practice(){
+        System.out.println("this a test");
+    }
+}
